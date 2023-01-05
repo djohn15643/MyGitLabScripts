f1668198c316908e00df5c81013d390179663533
John Doe <djohn15643@gmail.com>
2023-01-05 14:14:34 +0900

    get project

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
 import gitlab
 
+token = 'glpat-SXyvLbG39bct_FdBpbD_'  # This is sensitive data!!
+project_id = 42318536
+gl = gitlab.Gitlab(private_token=token)
+project = gl.projects.get(project_id)
+
+print(project.name)
