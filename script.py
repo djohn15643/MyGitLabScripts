2b791d80d9c85f5bfd4e9c052a08b835b13d9ba1
John Doe <djohn15643@gmail.com>
2023-01-05 14:20:43 +0900

    delete token

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import gitlab
 
-token = 'glpat-SXyvLbG39bct_FdBpbD_'  # This is sensitive data!!
+token = 'YOUR_API_TOKEN'  # This is sensitive data!!
 project_id = 42318536
 gl = gitlab.Gitlab(private_token=token)
 project = gl.projects.get(project_id)
